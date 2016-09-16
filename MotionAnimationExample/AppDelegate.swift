b37bc61181dbd0b98772152b3294846f942593bc
Luke <lzhaoyilun@gmail.com>
2016-09-16 11:00:31 -0400

    convert to latest swift 3 syntax

--- MotionAnimationExample/AppDelegate.swift
+++ MotionAnimationExample/AppDelegate.swift
@@ -12,15 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: Any]?) -> Bool {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
     window = UIWindow(frame: UIScreen.main.bounds)
     window!.rootViewController = ViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
 //    window!.rootViewController = SquareViewController()
     window!.makeKeyAndVisible()
     return true
   }
-
 
 }
 
